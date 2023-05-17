af26e74eb146d83fcaacba3f3ac0d8b4d9f5e9fe
antisyst <redmiramazan@gmail.com>
2023-05-17 11:55:45 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./navbar"
+function App() {
+  return (
+    <>
+     <Navbar/>
+     <div className="creator-content">
+      <h2>Developed by <a href="https://rmzn.netlify.app">Ramazan Azimli</a></h2>
+     </div>
+    </>
+  )
+}
+
+export default App;
+
+// imagine shooting a man with your last bullet and he stands there  UNFAZED
+
+   //                                                    Andrew Tate                                                   
+
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.scss'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+// imagine shooting a man with your last bullet and he stands there  UNFAZED
+
+   //                                                    Andrew Tate                                                   
+
+  //  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid non, cumque quidem nisi facilis illum tempore quas. At maiores magni ipsa tempora velit! Totam non sed quo quaerat, modi voluptatibus dicta error pariatur illo odit ea deleniti in deserunt recusandae animi tempora nam alias perferendis magni assumenda odio obcaecati nemo quidem architecto. Aliquid eos maiores fuga possimus deserunt aliquam, animi reprehenderit doloribus, aspernatur aperiam molestiae vitae corporis facere cum laborum necessitatibus? Molestias perspiciatis soluta pariatur ut amet ipsa. Optio maxime animi omnis possimus dignissimos vitae voluptatum dicta reprehenderit accusamus, at numquam modi nobis dolorum, commodi nam totam quisquam rerum blanditiis exercitationem! Asperiores numquam error doloremque enim dignissimos. In sunt officiis quidem laborum minus modi explicabo accusamus aspernatur tenetur ducimus libero corporis aliquam quia, voluptatem vero beatae, illum non ex dolorem molestias nulla magni asperiores. Nemo odit eligendi, maxime quaerat rem perspiciatis libero corporis laboriosam rerum saepe ut hic eaque quod impedit perferendis tenetur cumque commodi beatae dolor ducimus ipsa? Saepe libero ratione, ex neque doloremque fugit corrupti fuga temporibus quos commodi ducimus perferendis quas illum a non rerum facere nam sed, quia itaque deleniti dolore at dolores. Placeat, omnis? Error ipsa modi possimus aspernatur quos vel nostrum et earum facilis quasi asperiores placeat quam hic cumque eos inventore, ab ex a. Harum, sunt quos consectetur, ab voluptas adipisci, cum iste in dicta tenetur obcaecati quas veritatis? Nisi illo, culpa soluta, officia commodi cumque unde excepturi, corrupti voluptatibus aliquam nostrum maiores qui obcaecati sapiente nihil autem? Ipsa, libero provident reprehenderit, in cupiditate laudantium quisquam similique fugit impedit veniam minima atque facilis nobis perspiciatis dolore possimus incidunt cum quos rem eaque enim voluptatibus. Id quis iure aliquam doloremque temporibus vel distinctio, pariatur ex dolore porro corporis at repudiandae sapiente nihil provident debitis eos similique amet laboriosam quisquam minima impedit! Eos, est dolores.
